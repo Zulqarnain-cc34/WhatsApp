00a8bc69e48ecb3c72b29e72fe1c49196af0a444
Zulqarnain123 <powerranger16918@gmail.com>
2020-08-28 05:29:27 +0500

    Whatsapp clone made with react.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBK7IJDViK0p13DFHv6qpISQStqWlRt5gQ",
+  authDomain: "whatsapp-clone-720c2.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-720c2.firebaseio.com",
+  projectId: "whatsapp-clone-720c2",
+  storageBucket: "whatsapp-clone-720c2.appspot.com",
+  messagingSenderId: "106327815948",
+  appId: "1:106327815948:web:fc645f9d0fa67d82ea7b29",
+  measurementId: "G-84CM9DFSYQ",
+};
