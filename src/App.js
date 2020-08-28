d00f79a15e0eff4a45aca2072258759ae0f2fe73
Zulqarnain123 <powerranger16918@gmail.com>
2020-08-28 12:17:34 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import Sidebar from "./Components/Sidebar.js";
 import Chat from "./Components/Chat";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-
+import Login from "./Components/Login.js";
+import { useStateValue } from "./StateProvider";
 function App() {
+  const [{ user }, dispatch] = useStateValue();
   return (
     <div className="app">
-      <div className="app__body">
-        <Router>
-          <Sidebar />
-          <Switch>
-            <Route path="/rooms/:roomId">
-              <Chat />
-            </Route>
-            <Route path="/">
-              <Chat />
-            </Route>
-          </Switch>
-        </Router>
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Switch>
+              <Route path="/rooms/:roomId">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Chat />
+              </Route>
+            </Switch>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Components/Login.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "../Components Css/login.css";
+import { Button } from "@material-ui/core";
+import { auth, provider } from "../firebase.js";
+import { useStateValue } from "../StateProvider";
+import { actionTypes } from "../reducer";
+function Login() {
+  const [{}, dispatch] = useStateValue();
+  const signin = () => {
+    auth
+      .signInWithPopup(provider)
+      .then((res) => {
+        dispatch({
+          type: actionTypes.SET_USER,
+          user: res.user,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <div className="login">
+      <div className="login__container">
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg"
+          alt="WhatsApp"
+        />
+        <div className="login__text">
+          <h1>Sign in to WhatsApp</h1>
+        </div>
+        <Button onClick={signin}>Sign in with Google</Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
